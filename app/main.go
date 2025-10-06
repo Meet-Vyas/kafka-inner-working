ccccf98225450ad5e1424980428313676b1ded1c
Meet Vyas <vyasmet@gmail.com>
2025-10-06 12:41:38 -0700

    [MV] added support for giving out apiVersions supported response
    taking the api version from the input and if the apiVersion is > 4 we give out errorCode 35. If the apiVersions is 0-4, we give it back as response. first 4 bytes - message length. next 4 bytes corrID. next 4 bytes - errorCode. next we are replying with apiKeysCount. here we are replying with apiVersions supported but we can also add other features if needed. apiVersion reply is 18 (2 bytes) and then next 2 byte 0 - minVersion supported, next 2 bytes -> 4 - maxVersion supported

--- app/main.go
+++ app/main.go
@@ -77,10 +77,31 @@ func handleConn(conn net.Conn) {
 		}
 	}
 
-	resp := make([]byte, 10)
-	binary.BigEndian.PutUint32(resp[0:4], 0)
+	const apiKeysCount = 1
+	bodyLen := 2 + 4 + apiKeysCount*(2+2+2) // error_code(2) + array_length(4) + entries(6 bytes each)
+	totalLen := 4 + bodyLen        // corr_id(4) + bodyLen
+
+	resp := make([]byte, 4 + totalLen) // first 4 bytes: message length
+	binary.BigEndian.PutUint32(resp[0:4], uint32(totalLen))
 	binary.BigEndian.PutUint32(resp[4:8], corrID)
-	binary.BigEndian.PutUint16(resp[8:10], uint16(errorCode))
+	
+	offset := 8
+	binary.BigEndian.PutUint16(resp[offset:offset+2], uint16(errorCode))
+	offset += 2
+
+	binary.BigEndian.PutUint32(resp[offset:offset+4], uint32(apiKeysCount))
+	offset += 4
+	
+	// API Key entry	
+	binary.BigEndian.PutUint16(resp[offset:offset+2], 18)
+	offset += 2
+
+	binary.BigEndian.PutUint16(resp[offset:offset+2], 0)
+	offset += 2
+
+	binary.BigEndian.PutUint16(resp[offset:offset+2], 4)
+	offset += 2
+
 	_, err = conn.Write(resp)
 	if err != nil {
 		fmt.Println("Error writing response: ", err)
